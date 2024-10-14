370444b84db7614fcb788e46f8810afc20ab46d5
sumanmanna134 <manna.suman134@gmail.com>
2024-10-14 14:55:36 +0530

    Add gradle install with path

--- install.ps1
+++ install.ps1
@@ -26,4 +26,23 @@ winget install Microsoft.VisualStudioCode --silent --accept-source-agreements --
 Write-Host "Installing OpenJDK 17..." -ForegroundColor Cyan
 winget install EclipseAdoptium.Temurin.17.JDK --silent --accept-source-agreements --accept-package-agreements
 
+# Gradle=7.4.2
+Write-Host "Installing Gradle 7.4.2..." -ForegroundColor Cyan
+$gradleVersion = "7.4.2"
+$gradleUrl = "https://services.gradle.org/distributions/gradle-$gradleVersion-bin.zip"
+$gradleZip = "$env:TEMP\gradle-$gradleVersion-bin.zip"
+$gradleDir = "C:\Gradle\gradle-$gradleVersion"
+Invoke-WebRequest -Uri $gradleUrl -OutFile $gradleZip
+Expand-Archive -Path $gradleZip -DestinationPath "C:\Gradle"
+Remove-Item -Force $gradleZip
+$gradleBin = "$gradleDir\bin"
+if (-not ($env:Path -like "*$gradleBin*")) {
+    Write-Host "Adding Gradle to the system PATH..." -ForegroundColor Cyan
+    [System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";$gradleBin", [System.EnvironmentVariableTarget]::Machine)
+    Write-Host "Gradle 7.4.2 added to the system PATH successfully." -ForegroundColor Green
+} else {
+    Write-Host "Gradle is already in the system PATH." -ForegroundColor Yellow
+}
+
+
 Write-Host "All installations completed successfully!" -ForegroundColor Green
